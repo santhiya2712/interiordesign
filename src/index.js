633d3fe98c9e9f61a293d092a2cddc29b8f553de
santhiya2712 <santhiya2712@gmail.com>
2021-11-23 12:12:03 +0530

    to add the img and section part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sectionone from './Sectionone';
+import Sectiontwo from './Sectiontwo';
+
 
 class Interiordesign extends React.Component{
   render() {
     return (
-
+      <div>
       <Sectionone />
+      <Sectiontwo />
+
+      </div>
+
+
     
  
       
