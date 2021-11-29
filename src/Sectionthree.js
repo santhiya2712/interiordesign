51e165d7781c0289ffc1ce03562f518cec93a1cc
santhiya2712 <santhiya2712@gmail.com>
2021-11-29 19:27:30 +0530

    to add the style in sectiontwopage again to rearrange

--- src/Sectionthree.js
+++ src/Sectionthree.js
@@ -5,6 +5,14 @@ const Sectionthree = () => {
     return(
 		<div className={styles.Sectionthree}>
             <h3 className={styles.h3text}>What we offer?</h3>
+            <div className={styles.frame1}>
+          <div className={styles.block}>
+{/*            
+            <div>
+            <p className={styles.text}>Living room & Bedroom</p>
+            </div> */}
+          </div>
+        </div>
             
             
         </div>
